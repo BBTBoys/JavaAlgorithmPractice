3e88b62081648b032941bf11723eddcbc8479ecd
seokwangseok <seok_1219@hanmail.net>
2013-10-13 19:07:01 +0900

    add Quick Sort Algorithm (with simple type)

--- flower/src/main/java/net/adman/flower/web/controller/SortAlgorithmController.java
+++ flower/src/main/java/net/adman/flower/web/controller/SortAlgorithmController.java
@@ -1,14 +1,37 @@
 package net.adman.flower.web.controller;
 
+import java.util.List;
+
+import net.adman.flower.service.SortService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("sort_algorithm")
 public class SortAlgorithmController {
+	
+	@Autowired private SortService sortService;
 
 	@RequestMapping("")
 	public String home() {
 		return "sort/home";
+	}
+	
+	@RequestMapping(value="simple_sort", method=RequestMethod.GET)
+	public String simpleSort(Model model,
+			@RequestParam("values") List<Integer> values) {
+		
+		if (values.size() > 1) {
+			model.addAttribute("rawDataList", values);
+			model.addAttribute("resultList", sortService.getSimpleSorting(values));
+			model.addAttribute("type", "simple_sort");
+		}
+		
+		return "sort/simple_sort";
 	}
 }
